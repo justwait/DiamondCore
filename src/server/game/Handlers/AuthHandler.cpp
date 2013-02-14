eeae3ccbb0ec2a5f020347ae79264c838b47d0b2
den <GID_92@INBOX.RU>
2013-02-14 17:35:15 +0400

    update auth handler

--- src/server/game/Handlers/AuthHandler.cpp
+++ src/server/game/Handlers/AuthHandler.cpp
@@ -22,26 +22,49 @@
 
 void WorldSession::SendAuthResponse(uint8 code, bool queued, uint32 queuePos)
 {
+    QueryResult classresult = LoginDatabase.PQuery("SELECT class, expansion FROM realm_classes WHERE realmId = %u", realmID);
+    QueryResult raceresult = LoginDatabase.PQuery("SELECT race, expansion FROM realm_races WHERE realmId = %u", realmID);
+    if (!classresult || !raceresult)
+    {
+        sLog->outError(LOG_FILTER_GENERAL, "AuthHandler SendAuthResponse could not get db realm_classes and realm_races");
+        return;
+    }
     WorldPacket packet(SMSG_AUTH_RESPONSE, 1 /*bits*/ + 4 + 1 + 4 + 1 + 4 + 1 + 1 + (queued ? 4 : 0));
-    packet.WriteBit(queued);
-    if (queued)
-        packet.WriteBit(0);
-
     packet.WriteBit(1);                                    // has account info
 
+    // account info
+    packet.WriteBits(classresult->GetRowCount(), 25);           // Activation count for classes
+    packet.WriteBit(0);
+    packet.WriteBit(0);
+    packet.WriteBits(0, 22);                                // Activate character template windows/button
+    packet.WriteBits(raceresult->GetRowCount(), 25);            // Activation count for races
+    packet.WriteBit(queued);                                     // IsInQueue
     packet.FlushBits();
 
-    // account info
-    packet << uint32(0);                                   // BillingTimeRemaining
-    packet << uint8(Expansion());                          // 0 - normal, 1 - TBC, 2 - WOTLK, 3 - CATA; must be set in database manually for each account
-    packet << uint32(0);
-    packet << uint8(Expansion());                          // Unknown, these two show the same
-    packet << uint32(0);                                   // BillingTimeRested
-    packet << uint8(0);                                    // BillingPlanFlags
+    do
+    {
+        Field* fields = raceresult->Fetch();
 
+        packet << fields[1].GetUInt8();
+        packet << fields[0].GetUInt8();
+    } while (raceresult->NextRow());
+
+    packet << uint32(0);
+    packet << uint32(0);
+    packet << uint8(0);
+    packet << uint8(Expansion());                          // Unknown, these two show the same
+    packet << uint8(Expansion());                          // Unknown, these two show the same
+
+    do
+    {
+        Field* fields = classresult->Fetch();
+
+        packet << fields[0].GetUInt8();
+        packet << fields[1].GetUInt8();
+    } while (classresult->NextRow());
+    
+    packet << uint32(0);
     packet << uint8(code);
-    if (queued)
-        packet << uint32(queuePos);                             // Queue position
 
     SendPacket(&packet);
 }
